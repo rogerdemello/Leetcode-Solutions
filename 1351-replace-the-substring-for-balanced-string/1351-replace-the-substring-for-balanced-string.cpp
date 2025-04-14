7d1b6dc24b7b016c7c4c62857db6a3aabab7271f
Roger Demello <164307708+rogerdemello@users.noreply.github.com>
2025-04-14 21:13:30 +0530

    Time: 19 ms (47.94%), Space: 10.6 MB (33.53%) - LeetHub

--- 1351-replace-the-substring-for-balanced-string/1351-replace-the-substring-for-balanced-string.cpp
+++ 1351-replace-the-substring-for-balanced-string/1351-replace-the-substring-for-balanced-string.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     int balancedString(string s) {
+        unordered_map<char, int> count;
+        for (char c : s) count[c]++;
         
+        int n = s.size();
+        int res = n;
+        int left = 0;
+
+        for (int right = 0; right < n; ++right) {
+            count[s[right]]--;
+
+            while (left < n &&
+                   count['Q'] <= n / 4 &&
+                   count['W'] <= n / 4 &&
+                   count['E'] <= n / 4 &&
+                   count['R'] <= n / 4) {
+                res = min(res, right - left + 1);
+                count[s[left]]++;
+                left++;
+            }
+        }
+        return res;
     }
 };
