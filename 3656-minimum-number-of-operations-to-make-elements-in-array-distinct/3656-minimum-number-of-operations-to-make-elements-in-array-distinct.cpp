8d310343ff8b433e17b7fc9ee4f847c0be086f85
Roger Demello <164307708+rogerdemello@users.noreply.github.com>
2025-04-14 21:21:01 +0530

    Time: 11 ms (26.95%), Space: 31.6 MB (18.17%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        
+        int l = 0;
+        int r = nums.size() - 1;
+        int count = 0;
+
+        while (l <= r) {
+            unordered_map<int, int> mp;
+            bool foundDuplicate = false;
+
+            for (int i = l; i <= r; i++) {
+                mp[nums[i]]++;
+                if (mp[nums[i]] > 1) {
+                    foundDuplicate = true;
+                    break;
+                }
+            }
+
+            if (foundDuplicate) {
+                l += 3; 
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        return count;
     }
 };
