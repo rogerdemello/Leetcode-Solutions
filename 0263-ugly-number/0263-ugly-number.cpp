50137f82b91fe3f64dfcea760d70d782f7779e6e
Roger Demello <164307708+rogerdemello@users.noreply.github.com>
2025-05-01 12:32:21 +0530

    Time: 0 ms (100%), Space: 7.5 MB (100%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        int t;
+        if(n==0)
+        {
+            return false;
+        }
+        while(n!=1 || n%2==0 || n%3==0 || n%5==0)
+        {
+            if(n%2==0)
+            {
+                n = n/2;
+            }
+            else if(n%3==0)
+            {
+                n = n/3;
+            }
+            else if(n%5==0)
+            {
+                n = n/5;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        return false;
+    }
+};
