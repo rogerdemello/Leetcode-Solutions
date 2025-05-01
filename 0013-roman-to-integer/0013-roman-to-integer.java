03a5f5790f33f34fad9e2174d87277f3121c3fd2
Roger Demello <164307708+rogerdemello@users.noreply.github.com>
2025-05-01 12:17:32 +0530

    Time: 3 ms (80.34%), Space: 44.4 MB (98.53%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int romanToInt(String s) {
+        int sum =0;
+        int[] roman = new int[128];
+
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
+        
+        for(int i=0; i+1<s.length(); i++)
+        {
+            if(roman[s.charAt(i)] < roman[s.charAt(i+1)])
+            {
+                sum -= roman[s.charAt(i)];
+            }
+            else
+            {
+                sum += roman[s.charAt(i)];
+            }
+        }
+
+        return sum + roman[s.charAt(s.length()-1)];
+    }
+}
