a9faaa9a0b068973f89aa39a60c0c8a03b41ee00
Roger Demello <164307708+rogerdemello@users.noreply.github.com>
2025-05-04 07:57:58 +0530

    Time: 0 ms (100%), Space: 7.8 MB (65.18%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int x = a ^ b; 
+        int y = (a & b) << 1; 
+        while (y != 0) {
+            int car = (x & y) << 1;
+            x = x ^ y;
+            y = car;
+        }
+        return x;
+    }
+};
