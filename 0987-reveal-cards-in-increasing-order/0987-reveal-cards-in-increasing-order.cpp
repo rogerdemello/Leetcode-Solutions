c15a67dbab7898bf8a49fbe5174b510967f1d3b1
Roger Demello <164307708+rogerdemello@users.noreply.github.com>
2025-05-16 21:44:18 +0530

    Time: 4 ms (5.85%), Space: 14.6 MB (5.05%) - LeetHub

--- 0987-reveal-cards-in-increasing-order/0987-reveal-cards-in-increasing-order.cpp
+++ 0987-reveal-cards-in-increasing-order/0987-reveal-cards-in-increasing-order.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
-        
+        deque<int> dq;
+        sort(deck.rbegin(), deck.rend());
+        for(auto i:deck)
+        {
+            if(!dq.empty())
+            {
+                dq.push_front(dq.back());
+                dq.pop_back();
+            }
+            dq.push_front(i);
+        }
+        return vector<int> (dq.begin(), dq.end());
     }
 };
