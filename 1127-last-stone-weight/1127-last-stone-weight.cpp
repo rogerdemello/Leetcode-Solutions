213eba252f22079885af22a84609eb7435cdef0d
Roger Demello <164307708+rogerdemello@users.noreply.github.com>
2025-05-20 22:41:58 +0530

    Time: 0 ms (100%), Space: 9.8 MB (96.39%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> heap(stones.begin(), stones.end());
+        while(heap.size()>1)
+        {
+            int a = heap.top();
+            heap.pop();
+            int b = heap.top();
+            heap.pop();
+
+            if(a!=b)    heap.push(abs(a-b));
+        }        
+        return heap.empty()?0:heap.top();
+    }
+};
